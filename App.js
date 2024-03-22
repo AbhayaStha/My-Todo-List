ad2193207357129edb4242f51a5df761b4999a0e
AbhayaStha <69980750+AbhayaStha@users.noreply.github.com>
2024-03-22 13:40:54 +1100

    Implementation of the static main page

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.content}>
+        <Text style={styles.title}>My Todo List</Text>
+        <View style={styles.separator}></View>
+        <View style={styles.todoList}>
+            <View style={styles.todoItem}>
+            <Text style={styles.todoText}>Buy milk</Text>
+          </View>
+          <View style={styles.todoItem}>
+            <Text style={styles.todoText}>Buy bread</Text>
+          </View>
+          <View style={styles.todoItem}>
+            <Text style={styles.todoText}>Buy eggs</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.separator}></View>
+      <TouchableOpacity style={styles.addButton}>
+        <Text style={styles.buttonText}>ADD NEW TODO</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -15,6 +32,51 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+  content: {
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    alignSelf: 'center',
+  },
+  separator: {
+    width: '100%',
+    height: 1,
+    backgroundColor: '#000',
+    marginVertical: 10,
+  },
+  todoList: {
+    alignItems: 'stretch',
+    marginBottom: 10,
+    alignSelf: 'stretch',
+  },
+  todoItem: {
+    marginBottom: 5,
+    backgroundColor: '#f0f0f0',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  todoText: {
+    fontSize:14,
+  },
+  addButton: {
+    backgroundColor: '#6D8777',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 5,
+    alignSelf: 'stretch',
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 12,
+    textAlign: 'center',
   },
 });
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# My-Todo-List
+First milestone for My-Todo-List
+
+This is the static main page of the application where the title is on the top, the central part has a list of to-do items and "Add New Todo" button at the bottom.
